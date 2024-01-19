b8785ec2e0a2e23193ad61ec71adee34557d197a
Bindu <Bindu@DESKTOP-MPRUJU8>
2024-01-19 13:01:10 +0530

    first commit

---
+++ binduproject/src/test/java/com/binduproject/Login.java
@@ -0,0 +1,12 @@
+package com.binduproject;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Login {
+@Test
+
+public void username() {
+	Reporter.log("idiot",true);
+}
+}
